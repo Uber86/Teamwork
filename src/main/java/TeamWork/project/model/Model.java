49934da2e2049b723640550cb7dcbe9aa1c3b701
Сергей <lockup86@rambler.ru>
2025-03-06 21:10:35 +0300

    update: model, refactor: interface

--- src/main/java/TeamWork/project/model/Model.java
+++ src/main/java/TeamWork/project/model/Model.java
@@ -1,7 +1,55 @@
 package TeamWork.project.model;
 
+import java.util.Objects;
+
 public class Model {
     public int id;
     public String name;
     public String text;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Model model = (Model) o;
+        return id == model.id && Objects.equals(name, model.name) && Objects.equals(text, model.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, text);
+    }
+
+    @Override
+    public String toString() {
+        return "Model{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
 }
--- src/main/java/TeamWork/project/repository/RecommendationRuleSet.java
+++ src/main/java/TeamWork/project/repository/RecommendationRuleSet.java
@@ -1,5 +1,11 @@
 package TeamWork.project.repository;
 
-public interface RecommendationRuleSet extends JdbcTemplate<Model, Long>{
+import TeamWork.project.model.Model;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecommendationRuleSet extends JdbcTemplate<Model, Long> {
+
     Long findById(long id);
 }
