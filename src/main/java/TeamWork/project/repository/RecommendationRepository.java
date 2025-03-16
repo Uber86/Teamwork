de511d2eb3b20d49c42faaf02ed11fbb9463d6a4
Aleksei <alexejsedow@gmail.com>
2025-03-16 12:44:12 +0300

    Переписал запросы

--- src/main/java/TeamWork/project/repository/RecommendationRepository.java
+++ src/main/java/TeamWork/project/repository/RecommendationRepository.java
@@ -15,13 +15,17 @@ public class RecommendationRepository {
     public RecommendationRepository(@Qualifier("recommendationsJdbcTemplate") JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
-    public Boolean isUserOf(UUID userId, ProductType productType){
-        String sql = "SELECT COUNT(*) FROM products  WHERE id = ? AND type = ?";
+
+    public Boolean isUserOf(UUID userId, ProductType productType) {
+        //String sql = "SELECT COUNT(*) FROM products  WHERE id = ? AND type = ?";
+        String sql = "SELECT EXISTS(SELECT 1 FROM transactions WHERE user_id = ? AND type IN (SELECT id FROM products WHERE type = ?))";
         Integer count = jdbcTemplate.queryForObject(sql, new Object[]{userId, productType.name()}, Integer.class);
         return count != null && count > 0;
     }
-    public int sum (UUID userId, ProductType productType, TransactionType transactionType){
-        String sql = "SELECT SUM(amount) FROM transactions WHERE user_id = ? AND product_id = ? AND type = ?";
+
+    public int sum(UUID userId, ProductType productType, TransactionType transactionType) {
+        //String sql = "SELECT SUM(amount) FROM transactions WHERE user_id = ? AND product_id = ? AND type = ?";
+        String sql = "SELECT SUM(amount) FROM transactions WHERE user_id = ? AND product_id = ? AND type IN (SELECT id FROM products WHERE type = ?  )";
         Integer totalAmount = jdbcTemplate.queryForObject(sql, new Object[]{userId, productType.name(), transactionType.name()}, Integer.class);
 
         return totalAmount != null ? totalAmount : 0;
