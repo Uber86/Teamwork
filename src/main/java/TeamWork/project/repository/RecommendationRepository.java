8873e42cec42da3b82c78de00cea9b45df3407f3
Сергей <lockup86@rambler.ru>
2025-03-22 11:30:14 +0300

    Дописал методы 3 из 4

--- src/main/java/TeamWork/project/repository/RecommendationRepository.java
+++ src/main/java/TeamWork/project/repository/RecommendationRepository.java
@@ -20,37 +20,60 @@ public class RecommendationRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    /*
+    Метод о подтверждении у юзера продукта
+     */
     public Boolean isUserOf(UUID userId, ProductType productType) {
         //String sql = "SELECT COUNT(*) FROM products  WHERE id = ? AND type = ?";
         //"LEFT JOIN products p ON p.id = t.product_id WHERE t.user_id =? AND p.type =?"
-        String sql = "SELECT EXISTS(SELECT 1 FROM transactions WHERE user_id = ? AND type IN (SELECT id FROM products WHERE type = ?))";
+        String sql = "SELECT EXISTS(SELECT 1 " +
+                "FROM transactions WHERE user_id = ? " +
+                " type IN (SELECT id FROM products WHERE type = ?))";
         Integer count = jdbcTemplate.queryForObject(sql, new Object[]{userId, productType.name()}, Integer.class);
         return count != null && count > 0;
     }
 
+    /*
+    Метод суммы транзакции у юзера
+     */
     public int sum(UUID userId, ProductType productType, TransactionType transactionType) {
         //String sql = "SELECT SUM(amount) FROM transactions WHERE user_id = ? AND product_id = ? AND type = ?";
         //"LEFT JOIN products p ON p.id = t.product_id WHERE t.user_id =? AND p.type =? AND t.type=?"
-        String sql = "SELECT SUM(amount) FROM transactions WHERE user_id = ? AND product_id = ? AND type IN (SELECT id FROM products WHERE type = ?  )";
+        String sql = "SELECT SUM(amount) " +
+                "FROM transactions WHERE user_id = ? AND product_id = ? " +
+                "AND type IN (SELECT id FROM products WHERE type = ?  )";
         Integer totalAmount = jdbcTemplate.queryForObject(sql, new Object[]{userId, productType.name(), transactionType.name()}, Integer.class);
 
         return totalAmount != null ? totalAmount : 0;
     }
 
-    // Требуется дописать метод
+    /*
+    Метод для сравнения количества транзакции по продуктам
+    должен быть >5
+     */
     public boolean numberOfTransactions(UUID userId, ProductType productType ) {
-        String sql = "SELECT EXISTS(SELECT 1 FROM transactions WHERE user_id = ? AND type IN (SELECT id FROM products WHERE type = ?))";
+        String sql = "Select t.user_id, p.id " +
+                "From transactions t " +
+                "LEFT JOIN products p ON p.id = t.product_id " +
+                "WHERE t.user_id =? AND p.type =?";
         Integer count = jdbcTemplate.queryForObject(sql, new Object[]{userId, productType.name()}, Integer.class);
-        return count > 0;
+        return count > 5;
     }
 
-    // Используем константу для операторов сравнения, требуется дописать метод
+    /*
+    Метод для сравнения суммы транзакции определенного типа с константой
+     */
     public boolean TransactionSumCompare(UUID userId, ProductType productType,
                                          TransactionType transactionType,
                                          ComparisonOperators operators, int sum
     ) {
-        String sql = "";
-
+        String sql = "Select SUM(amount) " +
+                "From transactions t " +
+                "LEFT JOIN products p ON p.id = t.product_id " +
+                "WHERE t.user_id =? AND p.type =? AND t.type=?";
+        Integer count = jdbcTemplate.queryForObject(sql, new Object[]{userId, productType.name(), transactionType.name()}, Integer.class);
+        Boolean a = Boolean.valueOf(count + operators.name() + sum);
+        return a;
     }
 
     public boolean TransactionSumCompareDepositWithdraw(UUID userId, ProductType productType,
