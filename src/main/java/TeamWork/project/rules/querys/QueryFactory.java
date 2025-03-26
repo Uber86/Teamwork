dcdb894be5cee881a54000bddcfd89945708731b
Сергей <lockup86@rambler.ru>
2025-03-26 18:06:52 +0300

    Переправил метод Алексея в QueryFactory

--- src/main/java/TeamWork/project/rules/querys/QueryFactory.java
+++ src/main/java/TeamWork/project/rules/querys/QueryFactory.java
@@ -4,6 +4,8 @@ import TeamWork.project.dto.Querys;
 import TeamWork.project.repository.RecommendationRepository;
 import TeamWork.project.repository.RuleRepository;
 
+import java.util.Collections;
+
 
 public class QueryFactory {
 
@@ -13,13 +15,13 @@ public class QueryFactory {
     public static AbstractQuery from(Querys query, String arguments, boolean negate) {
         switch (query){
             case USER_OF:
-                return new  UserOfQuery(arguments,negate, ruleRepository);
+                return new  UserOf(Collections.singletonList(arguments), negate);
             case ACTIVE_USER_OF:
-                return new ActiveUserOfQuery(arguments, negate, ruleRepository);
+                return new ActiveUserOf(Collections.singletonList(arguments), negate);
             case TRANSACTION_SUM_COMPARE:
-                return new TransactionSumCompareQuery(arguments, negate, ruleRepository);
+                return new TransactionSumCompare(Collections.singletonList(arguments), negate);
             case TRANSACTION_SUM_COMPARE_DEPOSIT_WITHDRAW:
-                return new TransactionSumCompareDepositWithdrawQuery(arguments, negate, ruleRepository);
+                return new TransactionSumCompareDepositWithdraw(Collections.singletonList(arguments), negate);
         }
         return null;
     }
