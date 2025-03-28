855522140e8e1e00bb34030caa93e27cd84dc98a
Aleksei <alexejsedow@gmail.com>
2025-03-28 14:23:18 +0300

    Cache5 Исправлен RecommRepository

--- src/main/java/TeamWork/project/repository/RecommendationRepository.java
+++ src/main/java/TeamWork/project/repository/RecommendationRepository.java
@@ -2,6 +2,7 @@ package TeamWork.project.repository;
 
 import TeamWork.project.dto.ProductType;
 import TeamWork.project.dto.TransactionType;
+import com.github.benmanes.caffeine.cache.Cache;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -11,56 +12,63 @@ import java.util.UUID;
 @Repository
 public class RecommendationRepository {
     private final JdbcTemplate jdbcTemplate;
+    private final Cache<UUID, Boolean> userProductCache;
+    private final Cache<UUID, Integer> transactionSumCache;
+    private final Cache<UUID, Integer> transactionCountCache;
 
 
-    public RecommendationRepository(@Qualifier("recommendationsJdbcTemplate") JdbcTemplate jdbcTemplate) {
+    public RecommendationRepository(@Qualifier("recommendationsJdbcTemplate") JdbcTemplate jdbcTemplate,
+                                    Cache<UUID, Boolean> userProductCache,
+                                    Cache<UUID, Integer> transactionSumCache,
+                                    Cache<UUID, Integer> transactionCountCache) {
         this.jdbcTemplate = jdbcTemplate;
+
+        this.userProductCache = userProductCache;
+        this.transactionSumCache = transactionSumCache;
+        this.transactionCountCache = transactionCountCache;
     }
 
     /*
     Метод о подтверждении у юзера продукта
      */
     public Boolean isUserOf(UUID userId, ProductType productType) {
-        //String sql = "SELECT COUNT(*) FROM products  WHERE id = ? AND type = ?";
-        //"LEFT JOIN products p ON p.id = t.product_id WHERE t.user_id =? AND p.type =?"
-        String sql = "SELECT EXISTS(SELECT 1 " +
-                "FROM transactions WHERE user_id = ? " +
-                " type IN (SELECT id FROM products WHERE type = ?))";
-        Integer count = jdbcTemplate.queryForObject(sql, new Object[]{userId,
-                productType.name()}, Integer.class);
-        return count != null && count > 0;
+        return userProductCache.get(userId, key -> {
+            String sql = "SELECT EXISTS(SELECT 1 " +
+                    "FROM transactions WHERE user_id = ? " +
+                    "AND product_id IN (SELECT id FROM products WHERE type = ?))";
+            Integer count = jdbcTemplate.queryForObject(sql, new Object[]{userId, productType.name()}, Integer.class);
+            return count != null && count > 0;
+        });
     }
 
     /*
     Метод суммы транзакции у юзера
      */
     public int sum(UUID userId, ProductType productType, TransactionType transactionType) {
-        //String sql = "SELECT SUM(amount) FROM transactions WHERE user_id = ? AND product_id = ? AND type = ?";
-        //"LEFT JOIN products p ON p.id = t.product_id WHERE t.user_id =?
-        // AND p.type =? AND t.type=?"
-        String sql = "Select SUM(amount) " +
-        "From transactions t " +
-                "LEFT JOIN products p ON p.id = t.product_id " +
-                "WHERE t.user_id =? AND p.type =? AND t.type=?";
-        Integer totalAmount = jdbcTemplate.queryForObject(sql,
-                new Object[]{userId, productType.name()
-                        , transactionType.name()}, Integer.class);
-
-        return totalAmount != null ? totalAmount : 0;
+        return transactionSumCache.get(userId, key -> {
+            String sql = "SELECT SUM(amount) " +
+                    "FROM transactions t " +
+                    "LEFT JOIN products p ON p.id = t.product_id " +
+                    "WHERE t.user_id =? AND p.type=? AND t.type=?";
+            Integer totalAmount = jdbcTemplate.queryForObject(sql,
+                    new Object[]{userId, productType.name(), transactionType.name()}, Integer.class);
+            return totalAmount != null ? totalAmount : 0;
+        });
     }
 
     /*
     Метод для сравнения количества транзакции по продуктам
     должен быть >5
      */
-    public boolean numberOfTransactions(UUID userId, ProductType productType ) {
-        String sql = "Select count(t.*) " +
-                "From transactions t " +
-                "LEFT JOIN products p ON p.id = t.product_id " +
-                "WHERE t.user_id =? AND p.type =?";
-        Integer count = jdbcTemplate.queryForObject(sql, new Object[]{userId,
-                productType.name()}, Integer.class);
-        return count > 5;
+    public boolean numberOfTransactions(UUID userId, ProductType productType) {
+        return transactionCountCache.get(userId, key -> {
+            String sql = "SELECT COUNT(t.*) " +
+                    "FROM transactions t " +
+                    "LEFT JOIN products p ON p.id = t.product_id " +
+                    "WHERE t.user_id =? AND p.type=?";
+            Integer count = jdbcTemplate.queryForObject(sql, new Object[]{userId, productType.name()}, Integer.class);
+            return count != null ? count : 0;
+        }) > 5;
     }
 
     /*
