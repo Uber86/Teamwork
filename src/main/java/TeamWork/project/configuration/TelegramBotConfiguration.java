43d8449b58c6c75b2a72975afa262c17af5d3253
Сергей <lockup86@rambler.ru>
2025-03-29 15:54:52 +0300

    TelegramBot создание бота, запись токена в apl.pror. создание зависимости

---
+++ src/main/java/TeamWork/project/configuration/TelegramBotConfiguration.java
@@ -0,0 +1,19 @@
+package TeamWork.project.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfiguration {
+
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
+    }
+}
