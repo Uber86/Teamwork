f44184aa5afe8757dbe1c6bd2130af0fd4d579ec
Сергей <lockup86@rambler.ru>
2025-03-29 17:36:21 +0300

    Создал интерфейс команд, листенер команд

---
+++ src/main/java/TeamWork/project/command/TelegramCommand.java
@@ -0,0 +1,11 @@
+package TeamWork.project.command;
+
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+
+public interface TelegramCommand {
+
+    boolean support(Update update);
+
+    SendMessage handle(Update update);
+}
